c23ecd0a0becb0b207aa25402a786f2026c43e54
25sonu jena <pratibhajena288@gmail.com>
2024-09-16 09:52:25 +0000

    mongodone

--- sample.js
+++ sample.js
@@ -1,71 +1,21 @@
-const express = require('express')
-const mongoose = require('mongoose')
+const express = require('express');
+const connectDB = require('./config/db');
+// const bookRoutes = require('./routes/bookRoutes');
+
 const app = express();
-const port = 3000;
+const PORT = process.env.PORT || 3000;
 
-mongoose.connect('mongodb://localhost/mydatabase', {usernewurlparser: true, useunifiedtopology: true});
-
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-db.once('open', () => {
-  console.log('Connected to MongoDB successfully');
-});
-
-app.get('/', (req, res) => {
-  res.send('Hello World');
-});
-
-let books = [];
+// Connect to MongoDB
+connectDB();
 
 app.use(express.json());
 
-app.post('/books', (req, res) => {
-  const { title, author } = req.body;
-  if (!title || !author) {
-    return res.status(400).send('Missing title or author');
-  }
-
-  const newBook = { id: books.length + 1, title, author };
-  books.push(newBook);
-  res.status(201).send(newBook);
+app.get("/", (req, res) => {
+    res.send("HomePage Of The App");
 });
 
-  app.get('/books', (req, res) => {
-    const book = books.find(b => b.id === parseInt(req.params.id));
-    if (!book) {
-      return res.status(404).send('Book not found');
-    }
-    res.json(book);
-  });
+// app.use('/api', bookRoutes); // Use book routes with prefix '/api'
 
-  app.get('/books/:id', (req, res) => {
-    res.send("get by id request path is working")
-  });
-  
-  // Update a Book
-  app.put('/books/:id', (req, res) => {
-    const book = books.find(b => b.id === parseInt(req.params.id));
-    if (!book) {
-      return res.status(404).send('Book not found');
-    }
-  
-    const { title, author } = req.body;
-    book.title = title || book.title;
-    book.author = author || book.author;
-  
-    res.send(book);
-  });
-
-  app.delete('/books/:id', (req, res) => {
-    const bookIndex = books.findIndex(b => b.id === parseInt(req.params.id));
-  if (bookIndex === -1) {
-    return res.status(404).send('Book not found');
-  }
-
-  books.splice(bookIndex, 1);
-  res.status(204).send();
-});
-
-app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
+app.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`);
 });
